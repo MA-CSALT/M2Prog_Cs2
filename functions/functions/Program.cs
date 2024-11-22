e1fa979503405a2a22ea54cfc71768ffbc71dcc5
ruiter <38767@ma-web.nl>
2024-11-22 10:47:18 +0100

    03

--- functions/functions/Program.cs
+++ functions/functions/Program.cs
@@ -7,8 +7,59 @@
             Program program = new Program(); //program, Program
             program.Run(); //Run, void
         }
-        internal void Run() {
+        internal void Run()
+        {
             Console.WriteLine("Dit is nu de start van mijn programma");
+            Vraag1();
+            Vraag2();
+            Vraag3();
+            Vraag4();
+            Vraag5();
+            Vraag6();
         }
-    }
+        internal void Vraag1()
+        {
+            Console.WriteLine("How long do you think you'd survive in a zombie apocalypse?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+        internal void Vraag2()
+        {
+            Console.WriteLine("Why do round pizzas come in square boxes?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+        internal void Vraag3()
+        {
+            Console.WriteLine("Which species would be the rudest if all animals could talk?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+        internal void Vraag4()
+        {
+            Console.WriteLine("Has someone caught you dancing in front of the mirror?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+
+        internal void Vraag5()
+        {
+            Console.WriteLine("What is the worst game you played but that you liked anyway?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+        
+        internal void Vraag6()
+        {
+            Console.WriteLine("What secret conspiracy would you like to actually start letting other people know?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+     }
 }
