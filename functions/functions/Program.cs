6c1ee56a16d8bb77891242d496fdc6838b7833d3
ruiter <38767@ma-web.nl>
2024-11-22 10:22:00 +0100

    02

--- functions/functions/Program.cs
+++ functions/functions/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Program program = new Program(); //program, Program
+            program.Run(); //Run, void
+        }
+        internal void Run() {
+            Console.WriteLine("Dit is nu de start van mijn programma");
         }
     }
 }
