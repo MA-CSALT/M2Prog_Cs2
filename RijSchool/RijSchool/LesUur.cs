e912872e32fdce2d1125ec2b8c3d0abdaf56d2ae
ruiter <38767@ma-web.nl>
2024-12-03 09:53:06 +0100

    0201

--- RijSchool/RijSchool/LesUur.cs
+++ RijSchool/RijSchool/LesUur.cs
@@ -8,5 +8,9 @@ namespace RijSchool
 {
     internal class LesUur
     {
+        internal int tijd;
+        internal Student student;
+        internal RijLeraar rijLeraar;
+        internal Auto auto;
     }
 }
