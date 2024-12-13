859064e192fcff98f61c006e0f62960b37ba03a1
ruiter <38767@ma-web.nl>
2024-12-13 09:49:24 +0100

    0300

---
+++ Constructors/Constructors/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Run();
+        }
+
+        internal void Run()
+        {
+            QuizVraag quizvraag = new QuizVraag("hier komt de vraag.", "hier komt het antwoord.");
+        }
+    }
+}
