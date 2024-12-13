3ab795bd3c6aa0c0ad8b4e269eed08db8581109a
ruiter <38767@ma-web.nl>
2024-12-13 09:37:16 +0100

    0202 niet af

--- RijSchool/RijSchool/Program.cs
+++ RijSchool/RijSchool/Program.cs
@@ -4,7 +4,49 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Program program = new Program();
+            program.Run();
         }
+
+        internal void Run()
+        {
+            Auto auto1 = new Auto()
+            {
+                automaat = true,
+                kenteken ="AA824H",
+                kilometerStand= 20567,
+                merk="volkwagen"
+            };
+
+            Auto auto2 = new Auto()
+            {
+                automaat = false,
+                kenteken = "KU786G",
+                kilometerStand = 65739,
+                merk = "volvo"
+            };
+
+            RijLeraar rijLeraar = new RijLeraar()
+            {
+                leeftijd=30,
+                naam="John",
+                zzp= false
+            };
+
+            LesUur lesUur = new LesUur()
+            {
+                auto = auto1,
+                rijLeraar=rijLeraar,
+
+                tijd= 1130
+            };
+
+            Student student1 = new Student()
+            {
+                naam = "Nick",
+                leeftijd= 18,
+            };
+        }
+   
     }
 }
