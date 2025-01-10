9a2c2b5c199648ad33ede022b0c6e170cf1b0302
ruiter <38767@ma-web.nl>
2025-01-10 10:18:55 +0100

    0301

--- Constructors/Constructors/QuizVraagAntwoord.cs
+++ Constructors/Constructors/QuizVraagAntwoord.cs
@@ -10,5 +10,13 @@ namespace Constructors
     {
         internal QuizVraag vraag;
         internal bool goed;
+
+        internal QuizVraagAntwoord(QuizVraag vraag)
+        {
+            this.vraag = vraag;
+            goed = false;
+        }
     }
+
+
 }
